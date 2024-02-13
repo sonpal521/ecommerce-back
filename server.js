c97f9de79ca31b7ea8419c2400b508ca1bbf9f89
sonpal521 <119480099+sonpal521@users.noreply.github.com>
2024-02-13 22:46:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const authRoute = require('./routes/auth');
 
 //middleware
 const corsOptions = {
-	origin: 'https://shop-cart-wugy.vercel.app', // Replace with your React app's origin
+	origin: 'https://eco-shop-two.vercel.app', // Replace with your React app's origin
 	credentials: true, // Enable credentials (cookies) in cross-origin requests
 };
   
